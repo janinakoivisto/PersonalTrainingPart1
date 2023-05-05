9d1c3cc57860f7d9cd9d5b1badbc6cb777139782
Janina Koivisto <koivistokms@gmail.com>
2023-05-05 19:31:23 +0300

    Koodin muotoilua luettavammaksi

--- src/TrainingList.js
+++ src/TrainingList.js
@@ -5,28 +5,24 @@ import 'ag-grid-community/styles/ag-theme-alpine.css';
 import {Link} from 'react-router-dom';
 import {Box, Button, Container} from '@mui/material';
 
-
 const TrainingList = () => {
-  const [trainings, setTrainings] = useState([]);
+    const [trainings,
+        setTrainings] = useState([]);
 
-  useEffect(() => {
-    fetch('https://traineeapp.azurewebsites.net/gettrainings')
-      .then((response) => response.json())
-      .then((data) => {
-        const trainings = data.map((item) => ({
-          id: item.id,
-          date: new Date(item.date).toLocaleDateString() + ' ' + new Date(item.date).toLocaleTimeString(),
-          duration: item.duration,
-          activity: item.activity,
-          customer: `${item.customer.firstname} ${item.customer.lastname}`
-        }));
-        setTrainings(trainings);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-  
+    useEffect(() => {
+        fetch('https://traineeapp.azurewebsites.net/gettrainings').then((response) => response.json()).then((data) => {
+            const trainings = data.map((item) => ({
+                id: item.id,
+                date: new Date(item.date).toLocaleDateString() + ' ' + new Date(item.date).toLocaleTimeString(),
+                duration: item.duration,
+                activity: item.activity,
+                customer: `${item.customer.firstname} ${item.customer.lastname}`
+            }));
+            setTrainings(trainings);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }, []);
 
     const columnDefs = [
         {
@@ -44,16 +40,14 @@ const TrainingList = () => {
             field: 'activity',
             sortable: true,
             filter: true
-        },  {
-          headerName: 'Customer',
-          field: 'customer',
-          sortable: true,
-          filter: true
-      }
+        }, {
+            headerName: 'Customer',
+            field: 'customer',
+            sortable: true,
+            filter: true
+        }
 
     ];
-
-    
 
     return (
         <Container>
